0e944eb3b4a6b11b8757c74716f8de0490003183
Glen R. Goodwin <arei@usa.com>
2019-01-11 12:26:19 -0500

    Adds HelloBadge example.

---
+++ examples/HelloBadge/awesome-border-layout.js
@@ -0,0 +1,10 @@
+// (c) 2018, The Awesome Engineering Company, https://awesomeneg.com
+
+/* global name,from,html,css,onCreate,onAdd,onRemove,onAttribute */
+
+"use strict";
+
+name("awesome-border-layout");
+from(HTMLElement);
+html("./awesome-border-layout.html");
+css("./awesome-border-layout.css");
---
+++ examples/HelloBadge/awesome-hello-badge.js
@@ -0,0 +1,11 @@
+// (c) 2018, The Awesome Engineering Company, https://awesomeneg.com
+
+/* global name,from,html,css,onCreate,onAdd,onRemove,onAttribute */
+
+"use strict";
+
+requires("awesome-border-layout");
+
+name("awesome-hello-badge");
+html("./awesome-hello-badge.html");
+css("./awesome-hello-badge.css");
