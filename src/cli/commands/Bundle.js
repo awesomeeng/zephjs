9a499262a2d318fae9d2e1f2b7afcd1bbceae545
Glen R. Goodwin <arei@usa.com>
2019-04-04 16:28:58 -0400

    Fix for bundle cli command to download external url references.

--- src/cli/commands/Bundle.js
+++ src/cli/commands/Bundle.js
@@ -124,18 +124,54 @@ class Bundle extends AwesomeCLI.AbstractCommand {
 }
 
 const loader = function loader(url,rootDir,encoding="utf-8") {
-	if (url.startsWith("http:") || url.startsWith("https:") || url.startsWith("ftp:")) {
-		// load via request
-	}
-	else {
-		url = Path.resolve(rootDir,url);
-		return FS.readFileSync(url,{
-			encoding: encoding
-		});
-	}
+	return new Promise(async (resolve,reject)=>{
+		try {
+			if (url.startsWith("http:") || url.startsWith("https:") || url.startsWith("ftp:")) {
+				let tried = [];
+				let response = null;
+				while (true) {
+					if (tried.indexOf(""+url)>-1) return reject("URL redirect created a circular reference and could not be resolved.");
+					tried.push(url);
+
+					response = await AwesomeUtils.Request.get(url);
+
+					if (response.statusCode===200) break;
+					else if (response.statusCode>300 && response.statusCode<=308) {
+						let newurl = response.headers && response.headers["Location"] || response.headers && response.headers["location"] || null;
+						if (newurl) {
+							url = newurl;
+							response = null;
+							continue;
+						}
+						else {
+							return reject("URL did not return anything.");
+						}
+					}
+					else return reject("URL returned an error code: "+response.statusCode+": "+(response.status||""));
+				}
+
+				if (!response) return reject("URL did not return anything.");
+
+				let data = await response.content;
+				resolve(data);
+			}
+			else {
+				url = Path.resolve(rootDir,url);
+				FS.readFile(url,{
+					encoding: encoding
+				},(err,data)=>{
+					if (err) return reject(err);
+					resolve(data);
+				});
+			}
+		}
+		catch (ex) {
+			return reject(ex);
+		}
+	});
 };
 
-const inlineReferences = function inlineReferences(code,origin,quiet) {
+const inlineReferences = async function inlineReferences(code,origin,quiet) {
 	if (!quiet) console.log(origin);
 
 	let root = Path.dirname(origin);
@@ -147,194 +183,208 @@ const inlineReferences = function inlineReferences(code,origin,quiet) {
 
 	let offset = 0;
 	let paths = AwesomeUtils.Object.paths(nodes);
-	paths.forEach((path)=>{
+	await AwesomeUtils.Promise.series(paths,(path)=>{
 		if (!path) return;
 
-		let node = AwesomeUtils.Object.get(nodes,path);
-		if (typeof node==="object" && node.type && node.type==="CallExpression" && node.callee && node.callee.type==="Identifier") {
+		return new Promise(async (resolve)=>{
+			try {
+				let node = AwesomeUtils.Object.get(nodes,path);
+				if (typeof node==="object" && node.type && node.type==="CallExpression" && node.callee && node.callee.type==="Identifier") {
 
-			let revised = null;
-			if (node.callee.name==="html") revised = inlineHTML(origin,root,code,offset,node,quiet);
-			else if (node.callee.name==="css") revised = inlineCSS(origin,root,code,offset,node,quiet);
-			else if (node.callee.name==="image") revised = inlineImage(origin,root,code,offset,node,quiet);
-			if (revised) {
-				code = revised.code;
-				offset = revised.offset;
+					let revised = null;
+					if (node.callee.name==="html") revised = await inlineHTML(root,code,offset,node,quiet);
+					else if (node.callee.name==="css") revised = await inlineCSS(root,code,offset,node,quiet);
+					else if (node.callee.name==="image") revised = await inlineImage(root,code,offset,node,quiet);
+					if (revised) {
+						code = revised.code;
+						offset = revised.offset;
+					}
+				}
+
+				resolve();
 			}
-		}
+			catch (ex) {
+				console.error("\nERROR: "+origin+": "+ex);
+				process.exit();
+			}
+		});
 	});
 
 	return code;
 };
 
-const errorOut = function errorOut(origin,msg,exit=false) {
-	console.error("ERROR: "+origin+": "+msg);
-	if (exit) process.exit();
+const inlineHTML = function inlineHTML(root,code,offset,node,quiet) {
+	return new Promise(async (resolve,reject)=>{
+		try {
+			if (!node) return resolve();
+
+			if (!node.callee) return resolve();
+
+			if (!node.callee.type) return resolve();
+			if (node.callee.type!=="Identifier") return resolve();
+
+			if (!node.callee.name) return resolve();
+			if (node.callee.name!=="html") return resolve();
+
+			if (!node.arguments) return resolve();
+			if (!(node.arguments instanceof Array)) return resolve();
+
+			let args = node.arguments;
+			if (args.length<1) return resolve();
+
+			let arg = args[0];
+			if (!arg) return resolve();
+
+			if (!arg.type) return reject("html() statement was not syntactically valid.");
+			if (arg.type==="TemplateLiteral") return resolve(); // template literals are allowed for raw content.
+			if (arg.type!=="Literal") return reject("html() statement may only be a string literal.");
+			if (!arg.value) return resolve();
+
+			let start = arg.start;
+			let end = arg.end;
+			let length = arg.value.length;
+			let filename = arg.value;
+
+			let data = null;
+			if (filename.startsWith("http:") || filename.startsWith("https:") || filename.startsWith("ftp:")) {
+				if (!quiet) console.log("  "+filename);
+				data = await loader(filename);
+			}
+			else {
+				// data is not a filename and probably inline content.
+				filename = resolveFilename(root,filename,[".html",".htm"]);
+				if (!filename) return;
+
+				if (!quiet) console.log("  "+filename);
+				data = await loader(filename);
+			}
+			if (!data) return reject("Could not find inline reference to "+filename+".");
+
+			code = code.slice(0,start+offset)+"`"+data.replace(/`/g,"\\`")+"`"+code.slice(end+offset);
+			offset += (data.length-length);
+
+			resolve({code,offset});
+		}
+		catch (ex) {
+			return reject(ex);
+		}
+	});
 };
 
-const inlineHTML = function inlineHTML(origin,root,code,offset,node,quiet) {
-	if (!node) return;
+const inlineCSS = function inlineCSS(root,code,offset,node,quiet) {
+	return new Promise(async (resolve,reject)=>{
+		try {
+			if (!node) return resolve();
 
-	if (!node.callee) return;
+			if (!node.callee) return resolve();
 
-	if (!node.callee.type) return;
-	if (node.callee.type!=="Identifier") return;
+			if (!node.callee.type) return resolve();
+			if (node.callee.type!=="Identifier") return resolve();
 
-	if (!node.callee.name) return;
-	if (node.callee.name!=="html") return;
+			if (!node.callee.name) return resolve();
+			if (node.callee.name!=="css") return resolve();
 
-	if (!node.arguments) return;
-	if (!(node.arguments instanceof Array)) return;
+			if (!node.arguments) return resolve();
+			if (!(node.arguments instanceof Array)) return resolve();
 
-	let args = node.arguments;
-	if (args.length<1) return;
+			let args = node.arguments;
+			if (args.length<1) return resolve();
 
-	let arg = args[0];
-	if (!arg) return;
+			let arg = args[0];
+			if (!arg) return resolve();
 
-	if (!arg.type) return errorOut(origin,"html() statement was not syntactically valid.",true);
-	if (arg.type==="TemplateLiteral") return; // template literals are allowed for raw content.
-	if (arg.type!=="Literal") return errorOut(origin,"html() statement may only be a string literal.",true);
-	if (!arg.value) return;
+			if (!arg.type || !arg.value) return reject("css() statement was not syntactically valid.");
+			if (arg.type==="TemplateLiteral") return resolve(); // template literals are allowed for raw content.
+			if (arg.type!=="Literal") return reject("css() statement may only be a string literal.");
 
-	let start = arg.start;
-	let end = arg.end;
-	let length = arg.value.length;
-	let filename = arg.value;
+			let start = arg.start;
+			let end = arg.end;
+			let length = arg.value.length;
+			let filename = arg.value;
 
-	let data = null;
-	if (filename.startsWith("http:") || filename.startsWith("https:") || filename.startsWith("ftp:")) {
-		if (!quiet) console.log("  "+filename);
-		data = loader(filename);
-	}
-	else {
-		// data is not a filename and probably inline content.
-		filename = resolveFilename(root,filename,[".html",".htm"]);
-		if (!filename) return;
+			let data = null;
+			if (filename.startsWith("http:") || filename.startsWith("https:") || filename.startsWith("ftp:")) {
+				if (!quiet) console.log("  "+filename);
+				data = await loader(filename);
+			}
+			else {
+				// data is not a filename and probably inline content.
+				filename = resolveFilename(root,filename,[".css"]);
+				if (!filename) return;
 
-		if (!quiet) console.log("  "+filename);
-		data = loader(filename);
-	}
+				if (!quiet) console.log("  "+filename);
+				data = await loader(filename);
+			}
+			if (!data) return reject("Could not find inline reference to "+filename+".");
 
-	if (!data) {
-		console.error("ERROR: "+origin+": Could not find inline reference to "+filename+".");
-		process.exit();
-	}
+			code = code.slice(0,start+offset)+"`"+data.replace(/`/g,"\\`")+"`"+code.slice(end+offset);
+			offset += (data.length-length);
 
-	code = code.slice(0,start+offset)+"`"+data.replace(/`/g,"\\`")+"`"+code.slice(end+offset);
-	offset += (data.length-length);
-
-	return {code,offset};
+			resolve({code,offset});
+		}
+		catch (ex) {
+			return reject(ex);
+		}
+	});
 };
 
-const inlineCSS = function inlineCSS(origin,root,code,offset,node,quiet) {
-	if (!node) return;
+const inlineImage = function inlineImage(root,code,offset,node,quiet) {
+	return new Promise(async (resolve,reject)=>{
+		try {
+			if (!node) return resolve();
 
-	if (!node.callee) return;
+			if (!node.callee) return resolve();
 
-	if (!node.callee.type) return;
-	if (node.callee.type!=="Identifier") return;
+			if (!node.callee.type) return resolve();
+			if (node.callee.type!=="Identifier") return resolve();
 
-	if (!node.callee.name) return;
-	if (node.callee.name!=="css") return;
+			if (!node.callee.name) return resolve();
+			if (node.callee.name!=="image") return resolve();
 
-	if (!node.arguments) return;
-	if (!(node.arguments instanceof Array)) return;
+			if (!node.arguments) return resolve();
+			if (!(node.arguments instanceof Array)) return resolve();
 
-	let args = node.arguments;
-	if (args.length<1) return;
+			let args = node.arguments;
+			if (args.length<1) return resolve();
 
-	let arg = args[0];
-	if (!arg) return;
+			let arg = args.length===1 ? args[0] : args[1];
+			if (!arg) return resolve();
 
-	if (!arg.type || !arg.value) return errorOut(origin,"css() statement was not syntactically valid.",true);
-	if (arg.type==="TemplateLiteral") return; // template literals are allowed for raw content.
-	if (arg.type!=="Literal") return errorOut(origin,"css() statement may only be a string literal.",true);
+			if (!arg.type || !arg.value) return reject("image() statement was not syntactically valid.");
+			if (arg.type==="TemplateLiteral") return resolve(); // template literals are allowed for raw content.
+			if (arg.type!=="Literal") return reject("image() statement may only be a string literal.");
 
-	let start = arg.start;
-	let end = arg.end;
-	let length = arg.value.length;
-	let filename = arg.value;
+			let start = arg.start;
+			let end = arg.end;
+			let length = arg.value.length;
+			let filename = arg.value;
 
-	let data = null;
-	if (filename.startsWith("http:") || filename.startsWith("https:") || filename.startsWith("ftp:")) {
-		if (!quiet) console.log("  "+filename);
-		data = loader(filename);
-	}
-	else {
-		// data is not a filename and probably inline content.
-		filename = resolveFilename(root,filename,[".css"]);
-		if (!filename) return;
+			let data = null;
+			if (filename.startsWith("http:") || filename.startsWith("https:") || filename.startsWith("ftp:")) {
+				if (!quiet) console.log("  "+filename);
+				data = await loader(filename,filename,null);
+			}
+			else {
+				// data is not a filename and probably inline content.
+				filename = resolveFilename(root,filename,[".png",".gif",".jpg",".jpeg",".apng",".svg",".icon",".ico",".bmp"]);
+				if (!filename) return;
 
-		if (!quiet) console.log("  "+filename);
-		data = loader(filename);
-	}
+				if (!quiet) console.log("  "+filename);
+				data = await loader(filename,filename,null);
+			}
+			if (!data) return reject("Could not find inline reference to "+filename+".");
 
-	if (!data) {
-		console.error("ERROR: "+origin+": Could not find inline reference to "+filename+".");
-		process.exit();
-	}
+			data = "data:image/png;base64,"+data.toString("base64");
 
-	code = code.slice(0,start+offset)+"`"+data.replace(/`/g,"\\`")+"`"+code.slice(end+offset);
-	offset += (data.length-length);
+			code = code.slice(0,start+offset)+"`"+data+"`"+code.slice(end+offset);
+			offset += (data.length-length);
 
-	return {code,offset};
-};
-
-const inlineImage = function inlineImage(origin,root,code,offset,node,quiet) {
-	if (!node) return;
-
-	if (!node.callee) return;
-
-	if (!node.callee.type) return;
-	if (node.callee.type!=="Identifier") return;
-
-	if (!node.callee.name) return;
-	if (node.callee.name!=="image") return;
-
-	if (!node.arguments) return;
-	if (!(node.arguments instanceof Array)) return;
-
-	let args = node.arguments;
-	if (args.length<1) return;
-
-	let arg = args.length===1 ? args[0] : args[1];
-	if (!arg) return;
-
-	if (!arg.type || !arg.value) return errorOut(origin,"image() statement was not syntactically valid.",true);
-	if (arg.type==="TemplateLiteral") return; // template literals are allowed for raw content.
-	if (arg.type!=="Literal") return errorOut(origin,"image() statement may only be a string literal.",true);
-
-	let start = arg.start;
-	let end = arg.end;
-	let length = arg.value.length;
-	let filename = arg.value;
-
-	let data = null;
-	if (filename.startsWith("http:") || filename.startsWith("https:") || filename.startsWith("ftp:")) {
-		if (!quiet) console.log("  "+filename);
-		data = loader(filename,filename,null);
-	}
-	else {
-		// data is not a filename and probably inline content.
-		filename = resolveFilename(root,filename,[".png",".gif",".jpg",".jpeg",".apng",".svg",".icon",".ico",".bmp"]);
-		if (!filename) return;
-
-		if (!quiet) console.log("  "+filename);
-		data = loader(filename,filename,null);
-	}
-
-	if (!data) {
-		console.error("ERROR: "+origin+": Could not find inline reference to "+filename+".");
-		process.exit();
-	}
-
-	data = "data:image/png;base64,"+data.toString("base64");
-
-	code = code.slice(0,start+offset)+"`"+data+"`"+code.slice(end+offset);
-	offset += (data.length-length);
-
-	return {code,offset};
+			resolve({code,offset});
+		}
+		catch (ex) {
+			return reject(ex);
+		}
+	});
 };
 
 const resolveFilename = function resolveFilename(root,filename,extensions) {
